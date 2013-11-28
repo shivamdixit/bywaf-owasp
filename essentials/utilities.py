0c4c4fbab90ebc92312e3587e7e9eb47336ccc59
Noobay <shadowswatcher@gmail.com>
2013-11-28 09:56:19 +0200

    added a basic function to utilities.py

--- essentials/utilities.py
+++ essentials/utilities.py
@@ -2,3 +2,14 @@
 #bywaf utility methods, to be loaded on init
 ########################
 
+import urllib2
+
+def has_ssl(addr):
+    if('https://' not in addr):
+        addr = 'https://'+addr
+    else:
+        try:
+            urllib2.urlopen(addr)
+            return True
+        except urllib2.URLError:
+            return False
